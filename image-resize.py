8b9530437d7514e20b5ef2cc433ee90f6e92a16e
Alexei Broner <alexei.broner@gmail.com>
2021-07-20 16:21:35 -0500

    Add basic S3 caching-proxy functionality

--- image-resize.py
+++ image-resize.py
@@ -1,19 +1,26 @@
 from flask import Flask, send_from_directory
+import pathlib
 import os
 import urllib
 
 S3_BUCKET_URL = "https://s3-us-west-2.amazonaws.com/makersdistillery/"
-
-TEST_URL = "https://s3-us-west-2.amazonaws.com/makersdistillery/1000x/70c39d01b95bd38f265c22eba101de75_371055.jpg"
+IMAGES_PATH = pathlib.Path("images")
 
 app = Flask(__name__)
 
-@app.route("/images/<path:path>")
-def images(path):
-    if os.path.isfile(os.path.join("images", path)):
-        return send_from_directory("images", path)
-    else:
-        return "{} NOT FILE".format(path)
+@app.route("/images/<path:image_path>")
+def images(image_path):
+    if not os.path.isfile(os.path.join("images", image_path)):
+        print("not cached, saving to cache and then sending")
+        file_path = IMAGES_PATH.joinpath(*image_path.split("/")[0:-1])
+        file_name = os.path.basename(image_path)
+        file_path.mkdir(parents=True, exist_ok=True)
+
+        s3_url = S3_BUCKET_URL + image_path
+
+        urllib.request.urlretrieve(s3_url, file_path / file_name)
+
+    return send_from_directory("images", image_path)
 
 if __name__ == "__main__":
     app.run()
